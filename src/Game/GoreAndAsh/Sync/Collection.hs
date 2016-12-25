f4a919ad4ee400d3c275cd5faf9378f7baa57cf6
Anton Gushcha <ncrashed@gmail.com>
2016-12-25 03:51:17 +0300

    Finish implementation of hostCollection

--- src/Game/GoreAndAsh/Sync/Collection.hs
+++ src/Game/GoreAndAsh/Sync/Collection.hs
@@ -14,13 +14,13 @@ module Game.GoreAndAsh.Sync.Collection(
 
 import Control.Lens
 import Data.Bifunctor
-import Data.ByteString (ByteString)
 import Data.Map (Map)
 import Data.Monoid
 import Data.Store
-import GHC.Generics
 
 import qualified Data.Map.Strict as M
+import qualified Data.Set as S
+import qualified Data.Foldable as F
 
 import Game.GoreAndAsh
 import Game.GoreAndAsh.Logging
@@ -34,18 +34,17 @@ hostCollection :: forall k v v' a t m .
     ( Ord k, Store k, Store v', MonadAppHost t m
     , NetworkServer t m, SyncMonad t m)
   => SyncItemId -- ^ ID of collection in current scope
+  -> Dynamic t (S.Set Peer) -- ^ Set of peers that are allowed to get info about collection and that receives incremental updates.
   -> Map k v -- ^ Initial set of components
   -> Event t (Map k (Maybe v)) -- ^ Nothing entries delete component, Just ones create or replace
   -> (Peer -> v -> v') -- ^ Transform creation value to which is sent to remote clients
   -> (k -> v -> m a) -- ^ Constructor of widget
   -> m (Dynamic t (Map k a)) -- ^ Collected output of components
-hostCollection itemId initialMap addDelMap toClientVal makeComponent = do
+hostCollection itemId peersDyn initialMap addDelMap toClientVal makeComponent = do
   opts <- syncOptions
   let chan = opts ^. syncOptionsChannel
   -- resolve scope name
   i <- makeSyncName =<< syncCurrentName
-  -- watch after peers
-  peersDyn <- networkPeers
   -- collect contents of (key, start value) to send to remote peer with request
   rec kvMapDyn <- holdDyn initialMap $ flip pushAlways addDelMap $ \diffMap -> do
         kvMap <- sample . current $ kvMapDyn
@@ -55,19 +54,28 @@ hostCollection itemId initialMap addDelMap toClientVal makeComponent = do
   -- listen messages from client about the remote collections
   msgE :: Event t (Peer, RemoteCollectionMsg k v') <- listenCollectionMsg chan
   -- filter requests for current content of collection
-  let reqE = fforMaybe msgE $ \case
-        (peer, RemoteComponentsRequest{..}) -> if remoteComponentSyncId == i && remoteComponentItemId == itemId
-          then Just peer
-          else Nothing
-        _ -> Nothing
+  let reqE = flip push msgE $ \msg -> do
+        peers <- sample . current $ peersDyn
+        return $ case msg of
+          (peer, RemoteComponentsRequest{..}) ->
+            if   remoteComponentSyncId == i
+              && remoteComponentItemId == itemId
+              && peer `S.member` peers -- only peers that are in allowed set
+              then Just peer
+              else Nothing
+          _ -> Nothing
   -- when client wants to know current content of send the accummulated kvMapDyn
   _ <- msgSendMany $ flip pushAlways reqE $ \peer -> do
     kvMap <- sample . current $ kvMapDyn
     let mkMsg k v = (peer, chan, encodeComponentCreateMsg i itemId k $ toClientVal peer v)
     return $ M.mapWithKey mkMsg kvMap
-  -- TODO: send deletion and incremental addition of elements
-  -- hint: you can parametrise over Dynamic Peers to allow user to restrict peers set
-
+  -- send deletion and incremental addition of elements
+  let updMsgsE = flip pushAlways addDelMap $ \m -> do
+        peers <- sample . current $ peersDyn
+        let makeMsg peer k Nothing  = (peer, chan, encodeComponentDeleteMsg i itemId k)
+            makeMsg peer k (Just v) = (peer, chan, encodeComponentCreateMsg i itemId k $ toClientVal peer v)
+        return $ flip F.foldMap peers $ \peer -> M.elems $ M.mapWithKey (makeMsg peer) m
+  msgSendMany updMsgsE
   -- local collection
   holdKeyCollection initialMap addDelMap makeComponent
 
